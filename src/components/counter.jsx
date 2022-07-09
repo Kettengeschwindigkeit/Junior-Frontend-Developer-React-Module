8b1f49a136aa199d0dd21d95977c552e39e0afcd
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-09 10:46:36 +0300

    Dynamic classes render

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,23 @@
 import React from "react"
 
 const Counter = () => {
-    const count = 0 
+    const count = 7 
 
     const formatCount = () => {
         return count === 0 ? "empty" : count
     }
 
-    const imageUrl = "https://sun9-27.userapi.com/c408/u116869231/a_36b66556.jpg?ava=1"
+    const getBadgeClasses = () => {
+        let classes = "badge m-2 "
+        classes += count === 0 ? "bg-warning" : "bg-primary"
+        return classes
+    }
 
     return (
-        <React.Fragment>
-            <img src={imageUrl} alt="image" />
-            <span className="badge bg-primary m-2">{formatCount()}</span>
+        <>
+            <span className={getBadgeClasses()}>{formatCount()}</span>
             <button className="btn btn-primary btn-sm m-2">+</button>
-        </React.Fragment>
+        </>
     )
 }
 
