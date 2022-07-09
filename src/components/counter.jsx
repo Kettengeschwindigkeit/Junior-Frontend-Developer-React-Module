b633d29c0a10c369efdee21517bfe3f30e5d3779
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-09 10:41:05 +0300

    Set attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,11 +6,15 @@ const Counter = () => {
     const formatCount = () => {
         return count === 0 ? "empty" : count
     }
+
+    const imageUrl = "https://sun9-27.userapi.com/c408/u116869231/a_36b66556.jpg?ava=1"
+
     return (
-        <div>
-            <span>{formatCount()}</span>
-            <button>+</button>
-        </div>
+        <React.Fragment>
+            <img src={imageUrl} alt="image" />
+            <span className="badge bg-primary m-2">{formatCount()}</span>
+            <button className="btn btn-primary btn-sm m-2">+</button>
+        </React.Fragment>
     )
 }
 
