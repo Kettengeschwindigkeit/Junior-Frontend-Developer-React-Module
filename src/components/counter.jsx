dea14f17830d3ee560bf1e7c501328624bf745fa
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-11 15:44:57 +0300

    Array list render

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react"
 
 const Counter = () => {
 
-    const [count, setCount] = useState(0) 
+    const [count, setCount] = useState(0)
+    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
 
     const formatCount = () => {
         return count === 0 ? "empty" : count
@@ -22,8 +23,17 @@ const Counter = () => {
         setCount((prevState) => prevState - 1)
     }
 
+    const handleTagChange = () => {
+        setTags(['tag4', 'tag5'])
+    }
+
     return (
         <>
+            <ul>
+                {tags.map((tag) => (
+                    <li key={tag} className="btn btn-primary btn-sm m-2" onClick={handleTagChange}>{tag}</li>
+                ))}
+            </ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
             <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
             <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>-</button>
