ed5058368839830829bd25164a2ba6e6e4c3a05a
Kettengeschwindigkeit <666damned999@mail.ru>
2022-07-11 15:51:53 +0300

    Event arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -23,15 +23,15 @@ const Counter = () => {
         setCount((prevState) => prevState - 1)
     }
 
-    const handleTagChange = () => {
-        setTags(['tag4', 'tag5'])
+    const handleTagChange = (id) => {
+        setTags(prevState => prevState.filter(tag => tag !== id))
     }
 
     return (
         <>
             <ul>
                 {tags.map((tag) => (
-                    <li key={tag} className="btn btn-primary btn-sm m-2" onClick={handleTagChange}>{tag}</li>
+                    <li key={tag} className="btn btn-primary btn-sm m-2" onClick={() => handleTagChange(tag)}>{tag}</li>
                 ))}
             </ul>
             <span className={getBadgeClasses()}>{formatCount()}</span>
